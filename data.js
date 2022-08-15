14d4f639e025f058b75fc03e17ed55787cfdf8c3
Last One <107165373+darklordhereagain@users.noreply.github.com>
2022-08-15 22:40:29 +0600

    Update data.js

--- data.js
+++ data.js
@@ -2,21 +2,21 @@ const DATA = [
     {
         image: "https://play-lh.googleusercontent.com/ZglNaa1QiP_710cX7XhLDEHp-dYtfSwHRquuG5czm3InLMf2MXb2MQrhlef1lgtAgk0=w240-h480-rw",
         title: "BreakTheCode",
-        price: 10,
+        price: 40,
         info: "As quickly and in fewer moves as possible, here is the challenge! Break the secret code!",
         appUrl: "https://play.google.com/store/apps/details?id=com.sujon.BreakTheCode",
     },
     {
         image: "https://play-lh.googleusercontent.com/oKCZamG-YrkhG68cEMv43mPLf_FkNQjb4ImxO6N8gvsxEOCfRc2R8mJ5KvwXRv-Gbw=w240-h480-rw",
         title: "Track Your IP",
-        price: 12,
+        price: 50,
         info: "Search any IP Address in the World.",
         appUrl: "https://play.google.com/store/apps/details?id=com.sujon.ipinfo",
     },
     {
         image: "https://play-lh.googleusercontent.com/wYNJfEzBP4zmA5CW_iUGrbKcJMZl5Myj3IHmXNbkjCUMSL6xSlFMwAKAKgWPWYTdabM=w480-h960-rw",
         title: "Bangladesh 5G Browser",
-        price: 20,
+        price: 70,
         info: "Bangladesh 5G Browser is a new browser designed for Bangladeshi People. optimized for 5G networks. This browser is designed to give the user a better browsing experience, and to take advantage of the increased speed and lower latency of 5G networks. Bangladesh 5G Browser features advanced features like voice commands, which are perfect for your browsing",
         appUrl: "https://play.google.com/store/apps/details?id=com.bdbrowserreturn.browser",
     }
