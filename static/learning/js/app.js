1210c6e1958206e43949c7e52c44362b1cd48e8f
whoisjeeva <mytellee@gmail.com>
2024-05-04 06:42:25 +0530

    adding new courses

--- static/learning/js/app.js
+++ static/learning/js/app.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    if (location.hostname !== "localhost") {
-        setTimeout(function () {
-            fetch('https://www3.doubleclick.net', {
-                method: "HEAD",
-                mode: "no-cors",
-                cache: "no-store",
-            }).catch(() => {
-                document.querySelector("#message-dialog").style.display = "flex";
-                document.querySelector("#blocker-message").style.display = "flex";
-            });
-        }, 1000);
-    }
+    // if (location.hostname !== "localhost") {
+    //     setTimeout(function () {
+    //         fetch('https://www3.doubleclick.net', {
+    //             method: "HEAD",
+    //             mode: "no-cors",
+    //             cache: "no-store",
+    //         }).catch(() => {
+    //             document.querySelector("#message-dialog").style.display = "flex";
+    //             document.querySelector("#blocker-message").style.display = "flex";
+    //         });
+    //     }, 1000);
+    // }
 })
