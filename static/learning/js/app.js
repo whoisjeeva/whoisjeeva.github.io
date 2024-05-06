ed226cc1dcebf1c09adfa3f049574268295a8be8
whoisjeeva <mytellee@gmail.com>
2024-05-06 23:24:53 +0530

    adding new courses

--- static/learning/js/app.js
+++ static/learning/js/app.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    let fakeAd = document.createElement("div");
+    fakeAd.className = "textads banner-ads banner_ads ad-unit ad-zone ad-space adsbox"
+    fakeAd.style.height = "1px"
+
+    document.body.appendChild(fakeAd)
+    let x_width = fakeAd.offsetHeight;
+
+
+    if (x_width) {
+        document.querySelector("#blocker-message").style.display = "none";
+    } else {
+        document.querySelector("#blocker-message").style.display = "flex";
+    }
+
     // if (location.hostname !== "localhost") {
     //     setTimeout(function () {
     //         fetch('https://www3.doubleclick.net', {
